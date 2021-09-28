61143e9f30a23f66b99275d64cc0b9210ac05d40
86183 <309115015@qq.com>
2021-09-28 16:42:36 +0800

    添加3D选馆模型（老的主页中http://localhost:8080/library）

--- src/main/resources/static/main/js/pf.js
+++ src/main/resources/static/main/js/pf.js
@@ -1,15 +1,11 @@
 import * as THREE from '../three.js-master/build/three.module.js';
 
 import {OBJLoader} from '../three.js-master/examples/jsm/loaders/OBJLoader.js';
+import {MTLLoader} from '../three.js-master/examples/jsm/loaders/MTLLoader.js';
 
 let container;
 
 let camera, scene, renderer;
-
-let mouseX = 0, mouseY = 0;
-
-let windowHalfX = window.innerWidth / 2;
-let windowHalfY = window.innerHeight / 2;
 
 let canvasX = window.innerWidth / 2;
 let canvasY = window.innerHeight / 2;
@@ -19,14 +15,14 @@ let object;
 init();
 animate();
 
-
 function init() {
 
     container = document.createElement('div');
+    container.style.position = 'absolute';
     document.getElementById("outmap").appendChild(container);
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.z = 5;
+    camera.position.z = 42;
 
     // scene
     scene = new THREE.Scene();
@@ -37,15 +33,11 @@ function init() {
 
     // manager
     function loadModel() {
-        object.traverse(function (child) {
-            // if (child.isMesh) child.material.map = texture;
-        });
-
+        object.position.y = -4;
         scene.add(object);
     }
 
     const manager = new THREE.LoadingManager(loadModel);
-
     manager.onProgress = function (item, loaded, total) {
         console.log(item, loaded, total);
     };
@@ -61,54 +53,39 @@ function init() {
     function onError() {
     }
 
-    const loader = new OBJLoader(manager);
-    loader.load('main/model/pf.obj', function (obj) {
-        object = obj;
-    }, onProgress, onError);
+    const mtlLoader = new MTLLoader();
+    const objLoader = new OBJLoader(manager);
+    mtlLoader.load('main/model/pf.mtl', function (mtl) {
+        mtl.preload();
+        objLoader.setMaterials(mtl);
+        objLoader.load('main/model/pf.obj', function (obj) {
+            object = obj;
+        }, onProgress, onError);
+    });
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({alpha: true});
     renderer.setPixelRatio(window.devicePixelRatio);
     // canvas大小
     renderer.setSize(canvasX, canvasY);
     container.appendChild(renderer.domElement);
 
-    document.addEventListener('mousemove', onDocumentMouseMove);
-
     window.addEventListener('resize', onWindowResize);
 }
 
 function onWindowResize() {
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
     renderer.setSize(canvasX, canvasY);
 }
 
-function onDocumentMouseMove(event) {
-    if ((event.clientX <= canvasX && event.clientX >= 0) && (event.clientY <= canvasY && event.clientY >= 0)) {
-        mouseX = (event.clientX - windowHalfX) / 150;
-        mouseY = (event.clientY - windowHalfY) / 150;
-    }
-}
-
-//
-
 function animate() {
     requestAnimationFrame(animate);
     // 设置物体自动旋转
-    object.rotation.y += 0.01;
+    object.rotation.y = Date.now() * 0.001;
     render();
 }
 
 function render() {
-    camera.position.x += (mouseX - camera.position.x) * .05;
-    camera.position.y += (-mouseY - camera.position.y) * .05;
-
-    camera.lookAt(scene.position);
-
     renderer.render(scene, camera);
-
 }
