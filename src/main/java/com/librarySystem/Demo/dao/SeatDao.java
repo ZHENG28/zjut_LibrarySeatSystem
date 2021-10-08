a2574cdb1872f51a0fc16d8ffb46e1ea7f43d8fa
Syaro <1215791340@qq.com>
2021-10-08 20:33:58 +0800

    留言系统补充完成 登录页增加了回车键登录 用户登录页设置 select 默认为 “学生“ 找书页面删掉了”添加书本“的按钮

--- src/main/java/com/librarySystem/Demo/dao/SeatDao.java
+++ src/main/java/com/librarySystem/Demo/dao/SeatDao.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
+
 
 @Repository
 public interface SeatDao
