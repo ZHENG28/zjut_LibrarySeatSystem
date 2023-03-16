efb7ac78b9338d90c25b0828ebe963af65e024e6
Jillian <73706340+ZHENG28@users.noreply.github.com>
2023-03-16 14:34:00 +0000

    修改细节

--- src/main/java/com/librarySystem/Demo/entity/User.java
+++ src/main/java/com/librarySystem/Demo/entity/User.java
@@ -10,13 +10,16 @@ public class User
     private String id;
     private String name;
     private String password;
+    // 0-老师；1-学生
     private int identity;
+    // 0-男；1-女
     private int gender;
     private String campus;
     private String faculty;
     private String liketag;
     private int seatId;
     private Date reservetime;
+    // 0-未占座；1-已选座；2-暂离
     private int state;
     private int violate;
 
